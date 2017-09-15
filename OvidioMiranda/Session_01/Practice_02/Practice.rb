8989ba9d3a33c705e40687605d77a9d67c3f690c
OvidioMiranda <Ovidio.Miranda@fundacion-jala.org>
2017-09-15 16:01:33 -0400

    Practice-02-Script using arithmetic operators and comparison operators

---
+++ OvidioMiranda/Session_01/Practice_02/Practice.rb
@@ -0,0 +1,24 @@
+=begin
+*Create a script where you can print numbers and arithmetic result using comparison operators.
+ In this script, different calculations were performed using arithmetic operators
+ and comparison operators to better understand their use and application.
+=end
+
+# Arithmetic operators
+puts "8 + 9 = #{8 + 9}"
+puts "35 - 15 = #{35 - 15}"
+puts "3 * 9 = #{ 3 * 9}"
+puts "20 / 5 = #{20 / 5}"
+puts "22 % 3 = #{22 % 3}"
+puts "2 ** 8 = #{2 ** 8}"
+
+# Comparison operators
+puts "#{10} == #{5} ? #{10 == 5}"
+puts "#{10} != #{5} ? #{10 != 5}"
+puts "#{10} > #{5} ? #{10 > 5}"
+puts "#{10} < #{5} ? #{10 < 5}"
+puts "#{10} >= #{5} ? #{10 >= 5}"
+puts "#{10} <= #{5} ? #{10 <= 5}"
+puts "#{10} <=> #{5} ? #{10 <=> 5}"
+puts "(1...10) === #{5} ? #{(1...10) === 5}"
+puts "#{10}.eql?(#{10.0}) ? #{10.equal? 10.0}"
