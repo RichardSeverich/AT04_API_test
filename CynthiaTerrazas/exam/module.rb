0b7b481f0962b2ddcb13cba266c179b209051ae5
CynthiaTerrazas <cynthia.terrazas@fundacion-jala.org>
2017-09-20 23:17:40 +0000

    exam

---
+++ CynthiaTerrazas/exam/module.rb
@@ -0,0 +1,14 @@
+
+module ModuleA
+  def millimeters_to_centimeters(value)
+  return  value / 10
+  end
+
+  def centimeters_to_meters(value)
+    return value / 100
+  end
+
+  def meters_to_kilometers(value)
+    return value / 1000
+  end
+end
