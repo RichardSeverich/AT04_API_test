077da7b8249920035d121ce76ce95adcc11255c1
RichardSeverich <richard.severich@fundacion-jala.org>
2017-09-20 18:57:14 -0400

    Exam Solution

---
+++ RichardSeverich/Exam/module_convert.rb
@@ -0,0 +1,13 @@
+module Convert
+  def millimeters_to_centimeters(mil)
+  	mil/10.0
+  end
+  
+  def centimeters_to_meters(cent)
+  	cent/100.0
+  end
+
+  def metters_to_kilometers(met)
+  	met/1000.0
+  end
+end
