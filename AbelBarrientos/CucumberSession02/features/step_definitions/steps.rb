16d704e049921b45655b31b80e1725e08a7dda46
Abel <abelbs27101@gmail.com>
2017-09-26 19:40:07 -0400

    Solving first part of Session 02 practices

---
+++ AbelBarrientos/CucumberSession02/features/step_definitions/steps.rb
@@ -0,0 +1,31 @@
+Given(/^I am on "([^"]*)" page$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I search for information$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I click on "([^"]*)" button$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^The information is displayed$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I press Enter$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I can speak to perform the search$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^The important information is read$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I want to search random information$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
